d2e37c0df2a4ef90b779b17ae06ea5dabd2c4bc4
Volodymyr Koval <vovankoval1996@gmail.com>
2023-10-08 16:27:22 +0300

    done H/W

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,43 @@
+const inputNumber = document.querySelector('input[type="number"]');
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const boxesContainer = document.getElementById("boxes");
+
+// console.log(inputNumber);
+// console.log(createButton);
+// console.log(destroyButton);
+// console.log(boxesContainer);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  destroyBoxes();
+
+  let boxesHTML = "";
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const color = getRandomHexColor();
+    boxesHTML += `<div style="width: ${size}px; height: ${size}px; background-color: ${color};"></div>`;
+    size += 10;
+  }
+
+  boxesContainer.insertAdjacentHTML("beforeend", boxesHTML);
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+}
+
+createButton.addEventListener("click", () => {
+  const numberOfBoxes = inputNumber.value;
+  createBoxes(numberOfBoxes);
+});
+
+destroyButton.addEventListener("click", () => {
+  destroyBoxes();
+});
